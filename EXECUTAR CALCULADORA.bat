a4058d11c4639d0aadac66e85319c224d7a211c5
Anderson D <anderzdiogo27@gmail.com>
2025-11-13 13:19:52 -0300

    Atualizado o arquivo .bat para execução automática

--- EXECUTAR CALCULADORA.bat
+++ EXECUTAR CALCULADORA.bat
@@ -1,11 +1,5 @@
 @echo off
-echo ============================================
-echo  Calculadora de Carbono Avançada - UNIP
-echo ============================================
-echo.
-echo Verificando dependências...
+echo Iniciando Calculadora de Carbono...
 pip install -r requirements.txt
-echo.
-echo Iniciando a aplicação no navegador...
 streamlit run app/main.py
 pause
